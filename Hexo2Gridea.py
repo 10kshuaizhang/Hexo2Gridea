46303413145b04e34af24e9fff998e06b7dcf82a
10kshuaizhang <31811279+10kshuaizhang@users.noreply.github.com>
2023-02-12 17:18:38 +0800

    Update Hexo2Gridea.py
    refactoring powered be chatGPT

--- Hexo2Gridea.py
+++ Hexo2Gridea.py
@@ -7,26 +7,26 @@ HEXO_POST_DIR = "XXX/blog/source/_posts/"
 GRIDEA_POST_DIR = "XXX/Gridea/posts/"
 
 
-def substitute_metadata():
-    g = os.walk(HEXO_POST_DIR)
-    for _, _, file_list in g:
+def substitute_metadata(source_dir: str, target_dir: str):
+    for _, _, file_list in os.walk(source_dir):
         for filename in file_list:
-            if not filename.endswith("md"):
+            if not filename.endswith(".md"):
                 continue
-            source_file = os.path.join(HEXO_POST_DIR, filename)
-            target_file = os.path.join(GRIDEA_POST_DIR, filename)
-            with open(target_file, "wb+") as target:
-                with open(source_file, 'rb') as source:
-                    temp_list = source.read().split('\n'.encode("utf-8"))
-                    # 2. read the metadata and replace the hexo metadata with new Gridea metadata
-                    for i in range(len(temp_list)):
-                        line = temp_list[i] + '\n'.encode('utf-8')
-                        if i == 1:
-                            line = line[:7] + "'".encode() + line[7:-1] + "'".encode() + line[-1:]
-                        if i == 3:
-                            line += "published: true\nhideInList: false\nfeature: \nisTop: false\n".encode()
-                        target.write(line)
+            source_file = os.path.join(source_dir, filename)
+            target_file = os.path.join(target_dir, filename)
+            with open(target_file, "wb+") as target, open(source_file, 'rb') as source:
+                temp_list = source.read().split(b'\n')
+                # 2. read the metadata and replace the hexo metadata with new Gridea metadata
+                for i, line in enumerate(temp_list):
+                    line = line + b'\n'
+                    if i == 1:
+                        line = line[:7] + b"'" + line[7:-1] + b"'" + line[-1:]
+                    if i == 3:
+                        line += (
+                            b"published: true\nhideInList: false\nfeature: \nisTop: false\n"
+                        )
+                    target.write(line)
 
 
 if __name__ == "__main__":
-    substitute_metadata()
+    substitute_metadata(HEXO_POST_DIR, GRIDEA_POST_DIR)
